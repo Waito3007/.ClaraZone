8b365448279dde1e769beb043886d43f2c5797a3
AngSengZu <sangvu2015dp1@gmai.com>
2024-06-29 19:06:35 +0700

    Icon up

--- app/src/main/java/com/hoyo/cz/Activity/PostActivity.java
+++ app/src/main/java/com/hoyo/cz/Activity/PostActivity.java
@@ -20,7 +20,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
