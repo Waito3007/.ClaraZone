d3b0d532ace8c57c558e7a6cb3e17ac75fe7762f
AngSengZu <sangvu2015dp1@gmai.com>
2024-07-07 02:51:24 +0700

    cmt

--- app/src/main/java/com/hoyo/cz/Activity/MainActivity.java
+++ app/src/main/java/com/hoyo/cz/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,17 +29,18 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private DatabaseReference databaseReference;
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        //Kiểm tra nếu người dùng chưa đăng nhập
+        // Kiểm tra nếu người dùng chưa đăng nhập
         if (currentUser == null) {
             // Chuyển đến SignInActivity
             Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
-            finish(); //Đóng MainActivity để người dùng không thể quay lại bằng nút Back
+            finish(); // Đóng MainActivity để người dùng không thể quay lại bằng nút Back
         } else {
             // Kiểm tra thông tin người dùng
             databaseReference = FirebaseDatabase.getInstance().getReference("account").child(currentUser.getUid());
@@ -56,36 +58,43 @@ public class MainActivity extends AppCompatActivity {
                         initMainActivity();
                     }
                 }
+
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {}
             });
         }
     }
+
     private void initMainActivity() {
         setContentView(R.layout.activity_main);
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                int itemId = item.getItemId();
-                if (itemId == R.id.nav_home) {
-                    selectedFragment = new HomeFragment();
-                } else if (itemId == R.id.nav_search) {
-                    selectedFragment = new NotificationFragment();
-                } else if (itemId == R.id.nav_profile) {
-                    selectedFragment = new ProfileFragment();
-                }
-                if (selectedFragment != null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
-                    return true;
-                }
-                return false;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_search) {
+                selectedFragment = new SearchFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
             }
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment); // Load Fragment mới
+                return true;
+            }
+            return false;
         });
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // Replace Fragment hiện tại bằng Fragment mới
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null); // Để có thể quay lại Fragment trước đó bằng nút Back
+        transaction.commit();
     }
 }
